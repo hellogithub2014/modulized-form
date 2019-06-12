ac94ab642064ccafd6babf9d0ab563fe8ebce51d
liubin.frontend <liubin.frontend@bytedance.com>
2019-06-12 20:27:17 +0800

    zancun

--- src/store/base/base-form-item.js
+++ src/store/base/base-form-item.js
@@ -1,12 +1,18 @@
+import Vue from 'vue';
+
 export default {
   getters: {
-    isVisible ( state, getters, rootState, rootGetters ) {
-      // TODO: rootGetters.demo
-      // return rootGetters.isFormItemVisible( state._moduleKey );
-      return true;
+    isVisible ( state ) {
+      return !state._hidden;
     },
   },
   mutations: {
+    hide ( state ) {
+      Vue.set( state, '_hidden', true );
+    },
+    show ( state ) {
+      Vue.set( state, '_hidden', false );
+    },
     update ( state, newState ) {
       Object.keys( newState ).forEach( key => {
         state[ key ] = newState[ key ];
